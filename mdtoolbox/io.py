90908e8c1708b66657f53a0dcbed22c67e65589d
Daniel Bauer <bauer@cbs.tu-darmstadt.de>
2019-08-25 11:10:50 +0200

    xtc iterator

--- mdtoolbox/io.py
+++ mdtoolbox/io.py
@@ -25,3 +25,35 @@ def xvg2df(path):
     return pd.read_csv(path, delim_whitespace=True, header=header_rows,
                        index_col=index_col, names=col_names)
 
+
+def xtc_iterator(structure_file, trajectory_file, start=0, stop=None, step=None, batch_size=1000):
+    """ Allows to iterate over a trajectory in a memory friendly but less efficient way: it loads the trajectory
+    in chunks of batch_size and yields single frames from this batch until the batch is done.
+    Then, the moves to the next batch.
+    
+    Yields a tuple containing (timestep, AtomArray)
+    """
+    if step is None:
+        step = 1
+    if stop is None:
+        stop = np.inf
+    structure = io.load_structure(structure_file)
+    
+    batch_start = start
+    while batch_start < stop:  # iterate over batches
+        batch_stop = batch_start + batch_size*step
+        if batch_stop > stop:
+            batch_stop = stop
+#         print("start", batch_start, "stop", batch_stop, "step", step)
+        f = xtc.XTCFile()
+        f.read(trajectory_file, start=batch_start, stop=batch_stop, step=step)
+        time = f.get_time()
+        trj = f.get_structure(structure)
+        if len(trj) == 0:  # end of trj reached
+            return
+        for ts, frame in zip(time, trj):  # iterate over frames in batch
+            yield (ts, frame)
+        batch_start += batch_size*step
+        if batch_start > stop:
+            return
+        
